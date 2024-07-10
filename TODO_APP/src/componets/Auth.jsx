cec593de3bde3def71eec8e76afbfba5f1dcb0db
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-07-10 22:41:14 +0200

    authentication

--- TODO_APP/src/componets/Auth.jsx
+++ TODO_APP/src/componets/Auth.jsx
@@ -1,27 +1,9 @@
-import { useHistory } from 'react-router-dom';
+import { Link } from "react-router-dom"
+export default function Auth(){
+    return (
+        <Link to='/'>
+        <button>LogIn</button>
+        </Link>
+    )
 
-export default function Auth() {
-  const history = useHistory();
-
-  const handleLogin = () => {
-    // Simulating a login process (replace with actual logic)
-    const isLoggedIn = true; // Assume login is successful
-
-    if (isLoggedIn) {
-      // Redirect to home page (/home) after successful login
-      history.push('/home');
-    } else {
-      // Handle unsuccessful login (optional)
-      alert('Login failed. Please try again.');
-    }
-  };
-
-  return (
-    <div>
-      <h2>Login Page</h2>
-      <button onClick={handleLogin}>Log In</button>
-    </div>
-  );
 }
-
-
