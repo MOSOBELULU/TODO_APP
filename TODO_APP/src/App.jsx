ed456cf7043868f17686a4c6025952d99d93231b
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-07-16 22:26:37 +0200

    still struggling to debug react route

--- TODO_APP/src/App.jsx
+++ TODO_APP/src/App.jsx
@@ -1,32 +1,22 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Register from '../src/componets/Authentication/Register';
+import Register from '../src/componets/Authentication/Register'
 import NavBar from "./componets/Layout/NavBar";
 import "./App.css";
 import TodoPage from "./componets/TodoPage";
+import { Routes, Route } from "react-router-dom";
 import Login from "./componets/Authentication/Login";
-import { UseAuth } from "./contexts/authContext/index";
 
 function App() {
-  const { currentUser } = UseAuth();
-  const [isLoggedIn, setIsLoggedIn] = useState(!!currentUser);
-
   return (
-    <Router>
+    <>
+    <Routes>
+      <Route path="/login" element={<Login/>} />
+    </Routes>
+     <Routes>
+      <Route path="/register" element={ <Register />} />
+     </Routes>
       <NavBar />
-      <Routes>
-        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/register" element={<Register />} />
-        <Route 
-          path="/todo" 
-          element={isLoggedIn ? <TodoPage /> : <Navigate to="/login" />} 
-        />
-        <Route 
-          path="/" 
-          element={<Navigate to="/login" />} 
-        />
-      </Routes>
-    </Router>
+      <TodoPage/>
+    </>
   );
 }
 
--- TODO_APP/src/componets/Authentication/Register.jsx
+++ TODO_APP/src/componets/Authentication/Register.jsx
@@ -1,5 +1,5 @@
 import  { useState } from "react";
-import { useNavigate, Link  } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { doCreateUserWithEmailAndPassword } from "../../firebase/auth";
 
 export default function Register() {
