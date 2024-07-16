a34e2a81ebe2472c491a07e2c5777ec3d7af3b2c
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-07-16 22:40:09 +0200

    wip

--- TODO_APP/src/App.jsx
+++ TODO_APP/src/App.jsx
@@ -1,21 +1,30 @@
-import Register from '../src/componets/Authentication/Register'
-import NavBar from "./componets/Layout/NavBar";
-import "./App.css";
-import TodoPage from "./componets/TodoPage";
-import { Routes, Route } from "react-router-dom";
-import Login from "./componets/Authentication/Login";
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './componets/Authentication/Login';
+import Register from './componets/Authentication/Register';
+import NavBar from './componets/Layout/NavBar';
+import TodoPage from './componets/TodoPage';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to handle successful login or registration
+  const handleLoginOrRegister = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <>
-    <Routes>
-      <Route path="/login" element={<Login/>} />
-    </Routes>
-     <Routes>
-      <Route path="/register" element={ <Register />} />
-     </Routes>
+      <Routes>
+        {/* Redirect to Login page if not logged in */}
+        {!isLoggedIn && <Route path="/" element={<Navigate to="/login" />} />}
+        {/* Login and Register routes */}
+        <Route path="/login" element={<Login onLogin={handleLoginOrRegister} />} />
+        <Route path="/register" element={<Register onRegister={handleLoginOrRegister} />} />
+        {/* TodoPage route */}
+        {isLoggedIn && <Route path="/" element={<TodoPage />} />}
+      </Routes>
       <NavBar />
-      <TodoPage/>
     </>
   );
 }
